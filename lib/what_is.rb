d57fdbcb2592722ab67f24b6beb23db706c903b3
Mark Chavez <markchav3z@gmail.com>
2015-09-02 15:15:53 +0800

    build minimal functionality

--- lib/what_is.rb
+++ lib/what_is.rb
@@ -1,13 +1,23 @@
 require "what_is/version"
+require "net/http"
+require "uri"
 
 module WhatIs
   class Define
+    DICT_API_KEY = "ae396fba-2435-4197-b780-c4b5485fec22"
+    THES_API_KEY = "c80b99cc-421c-4f86-bc50-45ef40b371fe"
+
     def initialize(word)
+      @word = word
       @has_definition = false
     end
 
     def define!
-      "sample"
+      thesaurus_endpoint = "http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/#{@word}?key=#{THES_API_KEY}"
+      uri = URI.parse(thesaurus_endpoint)
+      response = Net::HTTP.get_response(uri)
+
+      response.body
     end
 
     def has_definition?
--- spec/lib/what_is_spec.rb
+++ spec/lib/what_is_spec.rb
@@ -10,7 +10,10 @@ describe WhatIs do
 
   context "defining a word" do
     it "returns the meaning" do
+      definition = double("definition", body: "sample")
       result = WhatIs::Define.new("house")
+
+      Net::HTTP.stub(:get_response).and_return(definition)
       expect(result.define!).to eq "sample"
     end
   end
